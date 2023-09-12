b9792bd0372c0c248b502d6267447fafc8669160
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2023-09-12 14:19:59 +0700

    Initial commit

---
+++ app/src/main/java/com/reyndev/forage/data/ForageDatabase.kt
@@ -0,0 +1,13 @@
+package com.reyndev.forage.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+/**
+ * Room database to persist data for the Forage app.
+ * This database stores a [Forageable] entity
+ */
+// TODO: create the database with all necessary annotations, methods, variables, etc.
+abstract class ForageDatabase : RoomDatabase() {
+
+}
